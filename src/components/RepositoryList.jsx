b1c9806338de285127d6d62be2a2e758315560e9
Danilo Nogueira <danilo.nogueira1992@gmail.com>
2021-09-19 22:43:08 -0300

    feat: add API request to fetch repositories to render on screen

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,22 +1,25 @@
+import { useState, useEffect } from 'react'
 import { RepositoryItem } from './RepositoryItem'
 import '../styles/repositories.scss'
 
-const repository = {
-  name: 'unform',
-  description: 'Forms in React',
-  link: 'https://github.com/unform/unform'
-}
-
 export function RepositoryList () {
+
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    fetch('https://api.github.com/orgs/rocketseat/repos')
+      .then(response => response.json())
+      .then(setRepositories)
+  }, [])
+
   return (
     <section className="repository-list">
       <h1>Lista de repositorios</h1>
 
       <ul>
-        <RepositoryItem repository={repository}/>
-        <RepositoryItem repository={repository}/>
-        <RepositoryItem repository={repository}/>
-        <RepositoryItem repository={repository}/>
+        {repositories.map(repo => {
+          return <RepositoryItem repository={repo} key={repo.id}/>
+        })}
       </ul>
     </section>
   )
